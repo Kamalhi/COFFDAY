8c480def09c9f170f83e2e5f3a12fcb88974eeb1
A. Kamal Hidayat <80084933+Eren-Yeagerist@users.noreply.github.com>
2025-09-30 11:59:38 +0700

    update responsive

--- script.js
+++ script.js
@@ -7,3 +7,41 @@ window.addEventListener("scroll", function () {
     header.style.backgroundColor = "rgba(249, 249, 249, 0.4)";
   }
 });
+
+// untuk menu-icon toggle
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+const menuIcon = `
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+  />`;
+
+const closeIcon = `<path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M6 18 18 6M6 6l12 12"
+  />`;
+
+function toggleMenu() {
+  navMenu.classList.toggle("active");
+
+  const icon = menuToggle.querySelector("path");
+  if (navMenu.classList.contains("active")) {
+    menuToggle.innerHTML = closeIcon;
+  } else {
+    menuToggle.innerHTML = menuIcon;
+  }
+}
+
+menuToggle.addEventListener("click", toggleMenu);
+
+const menuItems = navMenu.querySelectorAll("a");
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+    menuToggle.innerHTML = menuIcon;
+  });
+});
